dfbc2e69bb4c0b6f64f94612d71002c6634e3809
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-22 00:11:48 -0500

    error solve of depedency

--- HardikKitchen/Startup.cs
+++ HardikKitchen/Startup.cs
@@ -8,10 +8,12 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using HardikKitchen.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Taste.DataAccess;
+
+
 
 namespace HardikKitchen
 {
