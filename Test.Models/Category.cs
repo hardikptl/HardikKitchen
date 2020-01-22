be2a4b5a25853fa5011da9e7a19c7cb5021777de
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-22 17:04:40 -0500

    created Category class with 3 properties.

---
+++ Test.Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Test.Models
+{
+    public class Category
+    {
+        //if property name ID byfault  it consider as primary key but you can specify 
+        //primary key by [key] attribute
+        [Key]
+        public int Id { get; set; }
+
+        //using annotation [require] class for validation [display] for error message for property.
+        [Required]
+        [Display(Name ="Category Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name ="Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
--- Test.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Test.Models
-{
-    public class Class1
-    {
-    }
-}
