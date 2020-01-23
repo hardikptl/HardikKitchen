6f1cccb4172852b6eebe1abd7190795b5fd1135b
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-22 22:06:42 -0500

    Upsert page razor Design

--- HardikKitchen/Pages/Admin/Category/Index.cshtml.cs
+++ HardikKitchen/Pages/Admin/Category/Index.cshtml.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Test.Models;
 
 namespace HardikKitchen
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
 
-        }
     }
 }
---
+++ HardikKitchen/Pages/Admin/Category/Upsert.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Test.Models;
+
+namespace HardikKitchen
+{
+    public class UpsertModel : PageModel
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpsertModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Category CategoryObj { get; set; }
+
+        public IActionResult OnGet(int? id)
+        {
+            CategoryObj = new Test.Models.Category();
+            if (id != null)
+            {
+                CategoryObj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+                if (CategoryObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+        }
+    }
+}
+
