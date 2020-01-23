6bf953868b734e4772ea08862188d6b2e7085e91
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-23 17:40:59 -0500

    Created IFoodTYpe interface and FoodType Repository with Upadte nd DropDownList Method

---
+++ Taste.DataAccess/Data/Repository/IRepository/IFoodTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Test.Models;
+
+namespace Taste.DataAccess.Data.Repository.IRepository
+{
+    public interface IFoodTypeRepository :IRepository<FoodType>
+    {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+
+        void Update(FoodType foodType);
+    }
+}
