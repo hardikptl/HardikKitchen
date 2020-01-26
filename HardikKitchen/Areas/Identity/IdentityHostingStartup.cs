93579cd34a45b525217b80309471fd72e89824c3
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-26 15:09:40 -0500

    Added Identity Pages under Area

---
+++ HardikKitchen/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Taste.DataAccess;
+
+[assembly: HostingStartup(typeof(HardikKitchen.Areas.Identity.IdentityHostingStartup))]
+namespace HardikKitchen.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
