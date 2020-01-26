4725b871a343bc81fea68943fec54a81f484c11a
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-26 16:22:47 -0500

    Creted static class sd for user type and roles and added to signup design

--- Test.Utility/SD.cs
+++ Test.Utility/SD.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Test.Utility
 {
-    class SD
-    {
-    }
+        /// <summary>
+        /// Define type of user with role 
+        /// </summary>
+        public static class SD
+        {
+
+            public const string ManagerRole = "Manager";
+            public const string FrontDeskRole = "Front";
+            public const string KitchenRole = "Kitchen";
+            public const string CustomerRole = "Customer";
+        }
+    
 }
