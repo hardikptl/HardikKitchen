898899c473027033438bbf17c5fe29e2d01492ce
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-26 18:20:07 -0500

    getting all user  and locing unlocking user in user page

--- HardikKitchen/wwwroot/js/User.js
+++ HardikKitchen/wwwroot/js/User.js
@@ -7,7 +7,7 @@ function loadList() {
     debugger;
     dataTable = $('#DT_load').DataTable({
         "ajax": {
-           
+
             "url": "/api/user",
             "type": "GET",
             "datatype": "json"
@@ -17,52 +17,56 @@ function loadList() {
             { "data": "email", "width": "25%" },
             { "data": "phoneNumber", "width": "25%" },
             {
-                "data": "id",
+              
+                "data": { id: "id", lockoutEnd: "lockoutEnd" },
                 "render": function (data) {
-                    return `<div class="text-center" >
-                        <a href="/Admin/category/upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer;width:100px;">
-                        <i class="far fa-edit"></i> Edit
-                        </a>
-                        <a class="btn btn-danger text-white" style="cursor:pointer;width:100px;" onclick=Delete('/api/category/'+${data})>
-                        <i class="far fa-trash-alt"></i> Delete
-                        </a>
+                    var today = new Date().getTime(); //gettig today date
+                    var lockout = new Date(data.lockoutEnd).getTime(); //lockout date
+                    //comparing date to lock and unlock
+                    if (lockout > today) {
+                        //currently user is locked
+                        return ` <div class="text-center">
+                                <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
+                                   <i class="fas fa-lock-open"></i> Unlock
+                                </a></div>`;
 
-                        </div>`;
+                    }
+                    else {
+
+                        return ` <div class="text-center">
+                                <a class="btn btn-success text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
+                                    <i class="fas fa-lock"></i> Lock
+                                </a></div>`;
+                    }
 
                 }, "width": "30%"
             }
-            
-        ], 
+
+        ],
         "language": {
             "emptytable": "no data found."
         }, "width": "100%"
-        
+
     });
 
 }
 
-function Delete(url) {
-    swal({
-        title: "Are you sure want to Delete?",
-        text: "You will not able to restore the data!",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true
-    }).then((willDelete) => {
-        if (willDelete) {
-            $.ajax({
-                type: "DELETE",
-                url: url,
-                success: function (data) {
-                    if (data.success) {
-                        toastr.success(data.message);
-                        dataTable.ajax.reload();
-                    }
-                    else {
-                        toastr.error(data.message);
-                    }
-                }
-            });
+function LockUnlock(id) {
+    debugger;
+    $.ajax({
+        type: "POST",
+        url: '/api/User',
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+            else {
+                toastr.error(data.message);
+            }
         }
     });
+
 }
