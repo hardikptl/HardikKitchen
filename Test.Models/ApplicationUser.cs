175d0bb986bad080a20e6553e0bc530dc788f53a
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-26 15:18:24 -0500

    Creted ApplicationUser model with some Proporties.

---
+++ Test.Models/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Test.Models
+{
+
+    /// <summary>
+    /// this class hnadles application user properties
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        //displaying full name from full name propertity
+        [Display(Name ="Full Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        //notmapped will not added to database when craete migration
+        [NotMapped]
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+}
