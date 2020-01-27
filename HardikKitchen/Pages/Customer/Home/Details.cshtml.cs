061692ca0182837ab6fedf35e548742d0f9ce880
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-27 16:00:23 -0500

    Details Get Method

---
+++ HardikKitchen/Pages/Customer/Home/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Test.Models;
+
+namespace HardikKitchen
+{   
+    [Authorize]
+    public class DetailsModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DetailsModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public ShoppingCart ShoppingCartObj { get; set; }
+
+        /// <summary>
+        /// Getting Detials of Item by item id 
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(int id)
+        {
+            ShoppingCartObj = new ShoppingCart()
+            {
+                //filter data by id and getting two propertiesusing include property define in repository class
+                MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(includeProperties: "Category,FoodType", filter: c => c.Id == id),
+                MenuItemId =id
+            };
+        }
+    }
+}
