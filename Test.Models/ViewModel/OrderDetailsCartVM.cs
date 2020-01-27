8b1796d30fe6d19bc8a7967e773b7ad4478ef6b7
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-27 18:30:38 -0500

    added OrderDetailsCart ViewModel and Razor index page for cart

---
+++ Test.Models/ViewModel/OrderDetailsCartVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Models.ViewModel
+{
+
+    /// <summary>
+    /// View Model For Cart details with all properties 
+    /// of order class.
+    /// </summary>
+   public class OrderDetailsCartVM
+    {
+        public List<ShoppingCart> listCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
