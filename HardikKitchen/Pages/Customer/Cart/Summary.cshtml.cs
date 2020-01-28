dbc8476cbdf993a934ff0d4fa7080c0d79bb925d
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-27 21:06:24 -0500

    Summary Razor page under Cart folder

---
+++ HardikKitchen/Pages/Customer/Cart/Summary.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Test.Models;
+using Test.Models.ViewModel;
+
+namespace HardikKitchen.Pages.Customer.Cart
+{
+    public class SummaryModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SummaryModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public OrderDetailsCartVM detailCart { get; set; }
+
+        public IActionResult OnGet()
+        {
+            detailCart = new OrderDetailsCartVM()
+            {
+                OrderHeader = new Test.Models.OrderHeader()
+            };
+
+            detailCart.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            IEnumerable<ShoppingCart> cart = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value);
+
+            if (cart != null)
+            {
+                detailCart.listCart = cart.ToList();
+            }
+
+            foreach (var cartList in detailCart.listCart)
+            {
+                cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(m => m.Id == cartList.MenuItemId);
+                detailCart.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
+            }
+
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(c => c.Id == claim.Value);
+            detailCart.OrderHeader.PickupName = applicationUser.FullName;
+            detailCart.OrderHeader.PickUpTime = DateTime.Now;
+            detailCart.OrderHeader.PhoneNumber = applicationUser.PhoneNumber;
+            return Page();
+
+        }
+
+    }
+}
