ef4f739beec9ffa5da7103f555d3a0df0db3cb93
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-27 20:15:22 -0500

    POST method handler for PLUS , MINUS and REMOVE qauntity of item

--- HardikKitchen/Pages/Customer/Cart/Index.cshtml.cs
+++ HardikKitchen/Pages/Customer/Cart/Index.cshtml.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Taste.DataAccess.Data.Repository.IRepository;
 using Test.Models;
 using Test.Models.ViewModel;
+using Test.Utility;
 
 namespace HardikKitchen.Pages.Customer.Cart
 {
@@ -54,6 +56,67 @@ namespace HardikKitchen.Pages.Customer.Cart
                 cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(m => m.Id == cartList.MenuItemId);
                 OrderDetailsCartVM.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
             }
+        }
+
+        /// <summary>
+        /// this onPostPlus for plus button post method
+        /// </summary>
+        /// <param name="cartId"></param>
+        /// <returns></returns>
+        public IActionResult OnPostPlus(int cartId)
+        {
+            //getting item by id and adding one value in count of item
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        /// <summary>
+        /// this is ONPOSTMINUS for minus button post method
+        /// </summary>
+        /// <param name="cartId"></param>
+        /// <returns></returns>
+        public IActionResult OnPostMinus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            
+            if (cart.Count == 1)
+            {
+                //this is for item quantity is one
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+
+                var cnt = _unitOfWork.ShoppingCart.
+                GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+            }
+            else
+            {
+                //or decrement count by one
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+                _unitOfWork.Save();
+
+            }
+
+
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        /// <summary>
+        /// this is ONPOSTRemove for Remove button post method        
+        /// <param name="cartId"></param>
+        /// <returns></returns>
+        public IActionResult OnPostRemove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+
+            var cnt = _unitOfWork.ShoppingCart.
+                               GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+            HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+            return RedirectToPage("/Customer/Cart/Index");
         }
     }
 }
