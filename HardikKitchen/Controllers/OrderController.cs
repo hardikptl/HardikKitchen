b2cde4bc7df354a477f3158a315cbcc9b12f4cfd
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-28 17:36:27 -0500

    created orderdetailsvm for all deatils of the order and configure get handler in ordercontroller.

---
+++ HardikKitchen/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Test.Models;
+using Test.Models.ViewModel;
+using Test.Utility;
+
+namespace HardikKitchen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<OrderDetailsVM> orderListVM = new List<OrderDetailsVM>();
+
+            IEnumerable<OrderHeader> OrderHeaderList;
+
+            if (User.IsInRole(SD.CustomerRole))
+            {
+                //retrieve all orders for that customer
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                OrderHeaderList = _unitOfWork.OrderHeader.GetAll(u => u.UserId == claim.Value, null, "ApplicationUser");
+            }
+            else
+            {
+                OrderHeaderList = _unitOfWork.OrderHeader.GetAll(null, null, "ApplicationUser");
+            }
+
+            
+            foreach (OrderHeader item in OrderHeaderList)
+            {
+                OrderDetailsVM individual = new OrderDetailsVM
+                {
+                    OrderHeader = item,
+                    OrderDetails = _unitOfWork.OrderDetails.GetAll(o => o.OrderId == item.Id).ToList()
+                };
+                orderListVM.Add(individual);
+            }
+
+            return Json(new { data = orderListVM });
+        }
+    }
+}
---
+++ Test.Models/ViewModel/OrderDetailsVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Models.ViewModel
+{
+
+    /// <summary>
+    /// this viewmodel for order details
+    /// </summary>
+    public class OrderDetailsVM
+    {
+
+
+        public OrderHeader OrderHeader { get; set; }
+        public List<OrderDetails> OrderDetails { get; set; }
+    }
+
+}
