ec8972d1e58605b5c51b03e49e47ec65e3fd387d
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-28 20:41:53 -0500

    Manage order Razor page added get handler

---
+++ HardikKitchen/Pages/Admin/Order/ManageOrder.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Test.Models;
+using Test.Models.ViewModel;
+using Test.Utility;
+
+namespace HardikKitchen.Pages.Admin.Order
+{
+    public class ManageOrderModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ManageOrderModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public List<OrderDetailsVM> orderDetailsVM { get; set; }
+
+
+        public void OnGet()
+        {
+            orderDetailsVM = new List<OrderDetailsVM>();
+
+            List<OrderHeader> OrderHeaderList = _unitOfWork.OrderHeader
+                .GetAll(o => o.Status == SD.StatusSubmitted || o.Status == SD.StatusInProcess)
+                .OrderByDescending(u => u.PickUpTime).ToList();
+
+
+            foreach (OrderHeader item in OrderHeaderList)
+            {
+                OrderDetailsVM individual = new OrderDetailsVM
+                {
+                    OrderHeader = item,
+                    OrderDetails = _unitOfWork.OrderDetails.GetAll(o => o.OrderId == item.Id).ToList()
+                };
+                orderDetailsVM.Add(individual);
+            }
+        }
+
+    }
+}
